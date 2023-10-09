cb2929e4b64d337f62ce72eefd0f193c79c7250c
sohamdequantzent <sohamsdays@gmail.com>
2023-10-09 12:13:05 +0530

    Draw Paint feature

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef,useLayoutEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 const Board = () => {
   const canvasRef = useRef(null);
+  const shouldDraw = useRef(false);
   // state.menu.activeMenuItem gives me either eraser or brush
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   //based on the top code we set the bottom one
@@ -14,11 +15,47 @@ const Board = () => {
 
     const context = canvas.getContext("2d");
 
+    context.strokeStyle = color;
+    context.lineWidth = size;
+  }, [color, size]);
+
+  //before browser paint, before component mount
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+
+    const context = canvas.getContext("2d");
+
     // put canvas height and width as window
     // height and width
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     console.log(canvas.width);
+
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      context.beginPath(); // Start a new path
+      context.moveTo(e.clientX, e.clientY);
+    };
+    const handleMouseUp = (e) => {
+      
+      shouldDraw.current = false;
+    };
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) return;
+      context.lineTo(e.clientX, e.clientY);
+      context.stroke();
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
 
   console.log(color, size, 121);
